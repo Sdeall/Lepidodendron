7c9bce27f26611c34a6b8316eeba4139011c92f8
Sdeall <matviy.boshko@gmail.com>
2025-08-11 15:23:50 +0300

    Basic player movement

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -2,15 +2,55 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum MovementState {Static, Forward, Left, Right}
+
+    [SerializeField] float _moveSpeed, _rotationSpeed, _gravity;
+
+    MovementState currentState = MovementState.Static;
+
+    CharacterController controller;
+
+    Transform playerControllerTransform;
+
+    void Awake()
     {
-        
+        SetComponents();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        controller.Move(new Vector3(0, -_gravity * Time.fixedDeltaTime, 0));
+        if (currentState != MovementState.Static)
+            Move();
+    }
+
+    void Move()
+    {
+        switch (currentState)
+        {
+            case MovementState.Forward:
+                controller.Move(playerControllerTransform.forward * _moveSpeed * Time.fixedDeltaTime);
+                break;
+            case MovementState.Left:
+                playerControllerTransform.Rotate(Vector3.up, -_rotationSpeed * Time.fixedDeltaTime);
+                break;
+            case MovementState.Right:
+                playerControllerTransform.Rotate(Vector3.up, _rotationSpeed * Time.fixedDeltaTime);
+                break;
+        }
+    }
+
+    public void SetMovementState(MovementState state)
+    {
+        currentState = state;
+    }
+
+    void SetComponents()
+    {
+        controller = GetComponentInChildren<CharacterController>();
+        playerControllerTransform = controller.transform;
         
+        if (controller == null)
+            Debug.LogError("No CharacterController found on Player.");
     }
 }
---
+++ Assets/Player/Scripts/UIControlZone.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIControlZone : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public PlayerMovement.MovementState zoneState;
+
+    private PlayerMovement playerMovement;
+
+    void Start()
+    {
+        SetComponents();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        playerMovement.SetMovementState(zoneState);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        playerMovement.SetMovementState(PlayerMovement.MovementState.Static);
+    }
+
+    void SetComponents()
+    {
+        playerMovement = GetComponentInParent<PlayerMovement>();
+        if (playerMovement == null)
+            Debug.LogError("PlayerMovement not found in scene!");
+    }
+}
