38d805322e06b09328bea7431d08294c2480cdcc
Sdeall <matviy.boshko@gmail.com>
2025-08-13 12:14:18 +0300

    Movement cursors

--- Assets/Player/Scripts/PlayerCamera.cs
+++ Assets/Player/Scripts/PlayerCamera.cs
@@ -11,13 +11,17 @@ using UnityEngine.UI;
 public class PlayerCamera : MonoBehaviour
 {
     [Header("RenderTexture settings")]
-    public int targetWidth = 320;
-    public int targetHeight = 180;
-    public float updateInterval = 0.20f;
-    public bool useTemporaryRT = false;
+    [SerializeField] int targetWidth = 320;
+    [SerializeField] int targetHeight = 180;
+    [SerializeField] float updateInterval = 0.20f;
+    [SerializeField] bool useTemporaryRT = false;
+
+    [SerializeField] bool useOptimizations;
+
+
 
     [Header("References")]
-    public RawImage renderOutput;
+    [SerializeField] RawImage renderOutput;
 
     Camera playerCamera;
     RenderTexture lowResRT;
@@ -28,14 +32,14 @@ public class PlayerCamera : MonoBehaviour
         targetWidth = Mathf.Max(8, targetWidth);
         targetHeight = Mathf.Max(8, targetHeight);
         updateInterval = Mathf.Max(0f, updateInterval);
-
-        if (!Application.isPlaying) return;
-
+    
+        if (!Application.isPlaying && useOptimizations) return;
+    
         if (playerCamera == null)
             playerCamera = GetComponentInChildren<Camera>();
-
+    
         if (playerCamera != null)
-            Recreate(targetWidth, targetHeight);
+            CreateRenderTexture();
     }
 
     void Awake()
@@ -150,7 +154,7 @@ public class PlayerCamera : MonoBehaviour
 
         while (updateInterval > 0f)
         {
-            if (!Application.isFocused || !gameObject.activeInHierarchy)
+            if (!Application.isFocused  && useOptimizations|| !gameObject.activeInHierarchy)
             {
                 yield return null;
                 continue;
@@ -168,7 +172,7 @@ public class PlayerCamera : MonoBehaviour
         if (lowResRT == null) CreateRenderTexture();
         if (lowResRT == null) return;
 
-        if (!Application.isFocused) return;
+        if (!Application.isFocused  && useOptimizations) return;
 
         playerCamera.Render();
     }
@@ -188,12 +192,11 @@ public class PlayerCamera : MonoBehaviour
         }
     }
 
-    public void Recreate(int width, int height)
+        public void Recreate(int width, int height)
     {
         targetWidth = Mathf.Max(8, width);
         targetHeight = Mathf.Max(8, height);
         CreateRenderTexture();
         if (renderOutput != null) renderOutput.texture = lowResRT;
-        ForceRenderOnce();
     }
 }
--- Assets/Player/Scripts/UIControlZone.cs
+++ Assets/Player/Scripts/UIControlZone.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class UIControlZone : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class UIControlZone : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler
 {
     public PlayerMovement.MovementState zoneState;
 
+    [SerializeField] Texture2D _cursor;
+
     private PlayerMovement playerMovement;
+    
 
     void Awake()
     {
         SetComponents();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Cursor.SetCursor(_cursor, new Vector2(0,0), CursorMode.Auto);
     }
 
     public void OnPointerDown(PointerEventData eventData)
